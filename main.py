78d092b9aeb8cac09972602ec098c85460ca2e29
Stanisalv Gorovoy <cnfc19932@gmail.com>
2016-06-14 17:42:18 +0700

    Add tests

--- main.py
+++ main.py
@@ -5,105 +5,82 @@ import json
 import re
 
 class WordProperties(object):
+
 	def  _remove_bad_char(self, text = ''):
 		"""Удаление плохих символов"""
 
 		if text != '':
 			self.text = text
-		k = re.findall(ur'(?u)\w+',self.text.decode('utf-8'))
-	#	print k
-	#	for i in k:
-	#		print i
-		return k
+		k = re.findall(ur'(?u)\w+[-]*\w+',self.text.decode('utf-8'))
+		self.text = " ".join(k)
+		#print self.text
 		
-		
+	def answer(self):
+		"""Формируем ответ"""
 
+		answer = {'geo' : self.find_propertie([u'geo']), 'person' : self.find_propertie([u'famn',u'persn',u'patrn']), 'abr' : self.find_propertie([u'abbr']), 'all': self.find_propertie([u'S'])}
+		return answer
 
 	def __init__(self, text = '', debug = False, inputFile = 'program/input.txt', outputFile = 'program/output.txt'):
-
+		self.text = ''
 		self.inputFile = inputFile 
 		self.outputFile = outputFile 
 		self.debug = debug
-		
+		self.result = {}
+
 		if text != '':
-			self.text = ''
+			self.text = text
+
+	def run(self):
+		if self.text != '':
+			self._remove_bad_char()
 			f = open(self.inputFile, 'w')
-			f.write(text.encode('utf-8'))
+			f.write(self.text.encode('utf-8'))
 			f.close()
-		
-	#	self.result = {}
-	#	self._run()
 
-
-	def  _run(self):
 		jsonAnswer = '[]'
-		answer = os.system(r'program/mystem ' + self.inputFile + ' ' + self.outputFile + ' -i -d --eng-gr -l --format json') 
-		if self.debug:
-			if not(answer == 0):
-				print "Error! Mystem answer " + str(answer)
-			else:
-				print "Success! Mystem success"
-		if (answer == 0):
-			try: 
-				jsonAnswer = open(self.outputFile, 'r').read()
-			except FileNotFoundError:
-				if self.debug:
-					print "Error! File " + self.outputFile + " not found"
-			output = json.loads(jsonAnswer)
+		file_text = open(self.inputFile, 'r')
+		if not(file_text.read() == ''):
 
-			#print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++==="
-			#for i in output:
-			#	print (i['analysis'][0]['lex'] if len(i['analysis']) else '')
-			#print "_______________________________________________________________"
+			answer = os.system(r'program/mystem ' + self.inputFile + ' ' + self.outputFile + ' -i -d --eng-gr -l --format json') 
+			if self.debug:
+				print "Ok! self.text not empty"
+				if not(answer == 0):
+					print "Error! Mystem answer " + str(answer)
+				else:
+					print "Success! Mystem success"
+			if (answer == 0):
+				try: 
+					jsonAnswer = open(self.outputFile, 'r').read()
+				except FileNotFoundError:
+					if self.debug:
+						print "Error! File " + self.outputFile + " not found"
+				output = json.loads(jsonAnswer)
 
-			try:
-				self.result = {(i['analysis'][0]['lex'] if len(i['analysis']) else i['text']):set(re.findall(r"[\w]+", (i['analysis'][0]['gr'] if len(i['analysis']) else ''))) for i in output} #Magic
-			except Exception:
-				if self.debug:
-					print "Error! Re modul error"
-		#print self.result
+				try:
+					self.result = {(i['analysis'][0]['lex'] if len(i['analysis']) else i['text']):set(re.findall(r"[\w]+", (i['analysis'][0]['gr'] if len(i['analysis']) else ''))) for i in output} 
+				except Exception:
+					if self.debug:
+						print "Error! Re modul error"
+			#print self.result
+		else:
+			if self.debug:
+				print "Warning! self.text empty"
+		file_text.close()
 
-	def findAllPropertie(self, propertie):
-		answerWordList = {}
+	def find_propertie(self, propertie):
+		"""Ищем слова с нужным свойством"""
+
+		answerWordList = []
 		for i in self.result:
-			#print set(propertie),self.result[i]
 			if (set(propertie) & self.result[i]) and len(i) > 2:
-				answerWordList[i] = self.result[i]
+				answerWordList.append(i) 
 				if self.debug:
 					print set(propertie) & self.result[i], i
 		if self.debug:
 			print "Success! Find in method 'findAllPropertie'.Finded result " + str(len(answerWordList)) + " in " + str(len(self.result)) + ' words'
+
 		return answerWordList
 
-	def findPropertiesOr(self, properties):
-		properties = set(properties)
-		answerWordList = {}
-		for i in self.result:
-			if (properties & self.result[i]) and len(i) > 2:
-				answerWordList[i] = self.result[i]
-				if self.debug:
-					print properties & self.result[i], i
-		if self.debug:
-			print "Success! Find in method 'findPropertiesOr'.Finded result " + str(len(answerWordList)) + " in " + str(len(self.result)) + ' words'
-		return answerWordList
-
-
-
-
-
-
-#a = WordProperties()
-
-#a._remove_bad_char(text = '23 ')
-
-
-#k = a.findPropertiesOr(['geo','persn','famn','abbr','patrn'])
-
-#for i in k:
-#	print i
-#print "++++++++++++++++++++++=="
-
-#k = a.findAllPropertie(['geo'])
-
-#for i in k:
-#	print i
+if __name__ == "__main__":
+	pass
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,92 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import unittest
 import main as m
-#import sys
 
 
-class removeBadChar(unittest.TestCase):
+class testWordProperties(unittest.TestCase):
 	def setUp(self):
-		#print sys.path
-#		a =  
 		pass
 
 	def tearDown(self):
-	#	print "----end test"
 		pass
-	def test_firsttest(self):
-		#pass
-		k = m.WordProperties()._remove_bad_char('23')
-		self.assertEqual([u'23'], k) 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(removeBadChar)
-unittest.TextTestRunner(verbosity=2).run(suite)
+	def test_geo_find(self):
+		data = {
+			'Москва':u'москва',
+			'Новосибирск':u'новосибирск',
+			'В киеве':u'киев',
+			'Госсовет Крыма выделил 226,5 млн руб. на переселение семей из зоны Керченского моста' : u'крым',
+			'Задержан мэр Горно-Алтайска ему предъявлено обвинение мошенничестве превышении полномочий' : u'горно-алтайск'
+		}
+
+		data_two_or_more = {
+			'Из Киева в Германию' : [u'германия',u'киев'],
+			'Россия перестала получать с Украины жизненно важный препарат мезатон' : [u'россия',u'украина']
+		}
+		for i in data:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(data[i], k.find_propertie([u'geo'])[0]) 
+
+		for i in data_two_or_more:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(set(data_two_or_more[i]), set(k.find_propertie([u'geo']))) 
+
+	def test_famn_find(self):
+		data = {'Путин' : u'путин',
+				'Владимирович' : u'владимирович',
+				'Попов' : u'попов',
+				'Обама' : u'обама',
+				'Мухамед' : u'мухамед'}
+
+		data_two_or_more = {
+			'Мухамед Али прилетел к Ангеле Меркель' : [u'мухамед',u'меркель']
+		}
+
+		for i in data:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(data[i], k.find_propertie([u'famn',u'persn',u'patrn'])[0]) 
+
+
+		for i in data_two_or_more:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(set(data_two_or_more[i]), set(k.find_propertie([u'famn',u'persn',u'patrn']))) 
+
+	def test_abbr_find(self):
+		data = {'РСДРП': u'рсдрп',
+				'СССР': u'ссср',
+				'КХЛ' : u'кхл'
+		}
+
+		data_two_or_more = {
+			'Председатель СССР прилетел в США' : [u'ссср',u'сша'],
+			'КГБ и ФСБ' : [u'кгб',u'фсб']
+		}
+
+		for i in data:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(data[i], k.find_propertie([u'abbr'])[0]) 		
+
+		for i in data_two_or_more:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(set(data_two_or_more[i]), set(k.find_propertie([u'abbr']))) 
+
+	def test_WordProperties(self):
+		data = {
+			'Путин' : {'person': [u'путин'], 'abr': [], 'geo': [], 'all': [u'путин']},
+			'Париж столица Франции' : {'person': [], 'abr': [], 'geo': [u'париж',u'франция'], 'all': [u'париж',u'франция',u'столица']},
+			'Владимир Владимирович Путин прибыл в Париж в сзязи с апокалипсисом' : {'person': [u'\u0432\u043b\u0430\u0434\u0438\u043c\u0438\u0440', u'\u043f\u0443\u0442\u0438\u043d', u'\u0432\u043b\u0430\u0434\u0438\u043c\u0438\u0440\u043e\u0432\u0438\u0447'], 'abr': [], 'geo': [u'\u043f\u0430\u0440\u0438\u0436'], 'all': [u'\u0432\u043b\u0430\u0434\u0438\u043c\u0438\u0440', u'\u0430\u043f\u043e\u043a\u0430\u043b\u0438\u043f\u0441\u0438\u0441', u'\u043f\u0443\u0442\u0438\u043d', u'\u0432\u043b\u0430\u0434\u0438\u043c\u0438\u0440\u043e\u0432\u0438\u0447', u'\u043f\u0430\u0440\u0438\u0436', u'\u0441\u0437\u044f\u0437\u0438']}
+		}
+
+		for i in data:
+			k = m.WordProperties(text = i)
+			k.run()
+			self.assertEqual(set(data[i]), set(k.answer())) 		
+
